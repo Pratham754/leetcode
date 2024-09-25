9c9894d868ddaa7bed5f8c8c76b368ffb38866ba
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-09-25 15:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (38.00%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         for(int i=0;i<nums.size();i++){
             if(nums[i] == val){
                 nums.erase(nums.begin()+i);
