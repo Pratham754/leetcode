2f5f808fa11c61a94b4b5a308c7f99b7973dc5ea
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-08 19:30:46 +0530

    Time: 8 ms (61.11%) | Memory: 19.8 MB (29.33%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int longest = 0, beg = 0, i=0;
         while(seats[i]==0){
             beg++;
