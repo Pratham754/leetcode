675bcfe8e4bd8248b1a728d6fb2316013358bb2b
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-09 07:48:49 +0530

    Time: 3 ms (42.94%) | Memory: 7.9 MB (55.34%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int open = 0, mismatch = 0;
         for(int i=0;i<s.length();i++){
             if(s[i]=='(') open++;
