7a152d5c78a0442a459267938dd1f6ba650d1cbc
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-11 19:45:04 +0530

    Time: 14 ms (99.25%) | Memory: 38.5 MB (47.57%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
         int n = nums.size();
         vector<int> op(n);
         op[0] = 1;
-        for(int i=1;i<n;i++){
-            op[i] = op[i-1] * nums[i-1];
+        for (int i = 1; i < n; i++) {
+            op[i] = op[i - 1] * nums[i - 1];
         }
         int right = 1;
-        for(int i=n-1;i>=0;i--){
+        for (int i = n - 1; i >= 0; i--) {
             op[i] *= right;
             right *= nums[i];
         }
         return op;
     }
 };
-
-auto init = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
