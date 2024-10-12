ff31f34283ee6935ce7a7573062b966fea182ade
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-13 04:49:58 +0530

    Time: 35 ms (98.68%) | Memory: 62.1 MB (27.33%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int ans = 0, l = 0, r = height.size()-1;
 
         while(l<r){
