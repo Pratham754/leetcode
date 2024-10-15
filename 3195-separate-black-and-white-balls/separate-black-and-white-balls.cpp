90aa41bafe17b664b4eab8275bb2c4c411ba921a
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-15 16:57:34 +0530

    Time: 11 ms (100.00%) | Memory: 15.7 MB (14.11%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
+        
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
         long ans = 0;
         int ones = 0;
 
