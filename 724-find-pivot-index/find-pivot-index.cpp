3ab9de596dabaf287ce8b71573156d3c11bc5645
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-16 16:56:21 +0530

    Time: 7 ms (98.98%) | Memory: 34.8 MB (31.98%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+
+        ios_base::sync_with_stdio(0);\
+        cin.tie(0);
+        cout.tie(0);
+     
         int sums = 0;
         for(int i=0;i<nums.size();i++){
             sums += nums[i];
