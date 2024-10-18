e15b83d5eda7f7162d912d82a1f05d848fcda8f5
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-19 00:27:09 +0530

    Time: 79 ms (22.22%) | Memory: 10.1 MB (82.14%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int n = nums.size();
         int m = 0, c = 0;
 
