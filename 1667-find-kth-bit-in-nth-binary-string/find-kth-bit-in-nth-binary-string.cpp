771ef8b6822046b20c7f36f787c080331b3dd2b5
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2024-10-20 00:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (64.16%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
+        
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
         if (n == 1) return '0';
         int length = (1 << n) - 1;
         int mid = length / 2 + 1;
