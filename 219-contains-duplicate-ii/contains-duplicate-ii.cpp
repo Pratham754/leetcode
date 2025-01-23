6871101b8c32145458f88ca033ff963b95101f1c
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-01-23 21:22:06 +0530

    Time: 48 ms (90.05%) | Memory: 76.5 MB (75.58%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         unordered_set<int> st;
         for(int i=0;i<nums.size();i++){
             if(st.find(nums[i]) != st.end()) return true;
