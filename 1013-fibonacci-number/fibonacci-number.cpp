aee6f22fc225fa2a122ad7d71246c498d87d5d72
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-01-25 21:58:46 +0530

    Time: 85 ms (23.01%) | Memory: 8 MB (17.15%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if(n<=1) return n;
         return fib(n-1) + fib(n-2);
     }
