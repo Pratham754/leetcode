be9bd007c7e28c29ea669f8e90d8d87f00cba5fe
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-01-28 22:34:25 +0530

    Time: 45 ms (11.98%) | Memory: 48.8 MB (25.54%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         if(nums.empty()) return {};
         vector<int>res;
         unordered_map<int,int> mp;
