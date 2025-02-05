c7bcb94ac693893b888a0c46db6ce580c958ecf3
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-02-06 00:13:20 +0530

    Time: 4 ms (57.94%) | Memory: 109.4 MB (78.26%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -12,6 +12,10 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         if(root==NULL) return 0;
 
         queue<TreeNode*> q;
