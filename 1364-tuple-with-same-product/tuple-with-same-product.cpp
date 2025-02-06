b340837f2e3124f2eaafa0749741483c1e476678
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-02-06 21:57:27 +0530

    Time: 318 ms (56.74%) | Memory: 85.5 MB (19.44%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         unordered_map<int,int>mp;
         int ans=0, n=nums.size();
         for(int i=0;i<n;i++){
