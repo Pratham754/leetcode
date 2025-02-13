68ea8a4f87217ff6db7837009165937b6d587c9f
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-02-13 21:07:34 +0530

    Time: 88 ms (86.21%) | Memory: 89.4 MB (96.29%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+        
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         priority_queue<int,vector<int>,greater<int>> pq;
         
         for(int num : nums){
