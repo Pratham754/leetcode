9ea8d7b72ca42458f256edac40b0d28cbfd16b73
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-03-09 12:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 126.5 MB (90.38%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         vector<bool> res;
-        vector<int> flipped(1,0);
-        for(int i=1,j=0;i<nums.size();i++){
-            if((nums[i] & 1) == (nums[i-1] & 1)) j++;
-            flipped.push_back(j);
+        int n = nums.size();
+        vector<int> flipped(n,0);
+        for(int i = 1; i < n; i++){
+            flipped[i] = flipped[i-1] + ((nums[i] & 1) == (nums[i-1] & 1));
         }
-        for(auto q : queries) res.push_back(flipped[q[0]] == flipped[q[1]]);
+        for(auto& q : queries) res.push_back(flipped[q[0]] == flipped[q[1]]);
         return res;
     }
 };
