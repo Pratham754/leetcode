b94518e0dfa37053177e8c037bdab4a0bc8d312d
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-03-20 15:38:39 +0530

    Time: 12 ms (89.93%) | Memory: 208 MB (63.96%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,38 +1,49 @@
 class Solution {
 public:
+vector <int> parent;
+int find(int x){
+    if(x==parent[x]){
+        return x;
+    }
+    return parent[x] = find(parent[x]);
+}
+void Union(int x,int y){
+    parent[y] = x;
+}
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<int> parent(n), min_path_cost(n, -1);
-        iota(parent.begin(), parent.end(), 0);
-
-        function<int(int)> find_root = [&](int node) {
-            if (parent[node] != node) {
-                parent[node] = find_root(parent[node]);
-            }
-            return parent[node];
-        };
-
-        for (auto& edge : edges) {
-            int source = edge[0], target = edge[1], weight = edge[2];
-            int source_root = find_root(source);
-            int target_root = find_root(target);
-
-            min_path_cost[target_root] &= weight;
-
-            if (source_root != target_root) {
-                min_path_cost[target_root] &= min_path_cost[source_root];
-                parent[source_root] = target_root;
-            }
+        parent.resize(n);
+        vector<int> cost(n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            cost[i] = -1;
         }
-
+        for(auto &edge:edges){
+            int u = edge[0];
+            int v = edge[1];
+            int p1 = find(u);
+            int p2 = find(v);
+            if(p1!=p2){
+                Union(p1,p2);
+                cost[p1]&=cost[p2];
+            }
+            int w = edge[2];
+            cost[p1]&=w;
+        }
         vector<int> result;
-        for (auto& q : query) {
-            int start = q[0], end = q[1];
-            if (start == end) {
+        for(auto &q:query){
+            int s = q[0];
+            int t = q[1];
+            int p1 = find(s);
+            int p2 = find(t);
+            if(s==t){
                 result.push_back(0);
-            } else if (find_root(start) != find_root(end)) {
+            }
+            else if(p1!=p2){
                 result.push_back(-1);
-            } else {
-                result.push_back(min_path_cost[find_root(start)]);
+
+            }
+            else{
+                result.push_back(cost[p1]);
             }
         }
         return result;
