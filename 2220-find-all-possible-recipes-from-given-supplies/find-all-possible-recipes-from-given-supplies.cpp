f54f1bf49967da26878c5cce8099d6ef37cd8032
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-03-21 08:08:16 +0530

    Time: 52 ms (95.02%) | Memory: 101.4 MB (88.95%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        map<string, vector<string>> adjList;
-        
-        map<string, int>indegree;
-        
-        for(int i=0; i<ingredients.size(); i++)
-        {
-            for(auto &item : ingredients[i])
-            {
-                adjList[item].push_back(recipes[i]);
-                
-                indegree[recipes[i]]++;
+    vector<string> findAllRecipes(const vector<string>& recipes, const vector<vector<string>>& ingredients, const vector<string>& supplies) {
+        const unordered_set have(supplies.cbegin(), supplies.cend());
+        const int n = (int)recipes.size();
+        unordered_map<string, int> mp;
+        for (int i = 0; i < n; i++)
+            mp[recipes[i]] = i;
+        vector<vector<int>> adj(n);
+        vector<int> degree(n);
+        for (int i = 0; i < n; i++) {
+            degree[i] = (int)ingredients[i].size();
+            for (const auto& s : ingredients[i]) {
+                if (have.contains(s))
+                    degree[i]--;
+                if (mp.contains(s))
+                    adj[mp[s]].push_back(i);
             }
         }
-        
-        queue<string>q;
-        
-        for(auto &x : supplies)
-            if(indegree[x]==0)
-                q.push(x);
-        
-        while(!q.empty())
-        {
-            string node = q.front();
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+            if (!degree[i])
+                q.push(i);
+        vector<string> ret;
+        while (!q.empty()) {
+            const auto u = q.front();
             q.pop();
-            
-            
-            for(auto &nei : adjList[node])
-            {
-                indegree[nei]--;
-                
-                if(indegree[nei]==0)
-                    q.push(nei);
-            }
+            ret.push_back(recipes[u]);
+            for (const auto v : adj[u])
+                if (--degree[v] == 0) q.push(v);
         }
-        
-        
-        vector<string> ans;
-        
-        for(auto &x : recipes)
-            if(indegree[x]==0)
-                ans.push_back(x);
-            
-        return ans;
+        return ret;
     }
 };
