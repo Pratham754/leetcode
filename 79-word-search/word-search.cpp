649585a2af72581739f258d9aa757989a71543d7
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-06-21 17:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (69.14%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx) {
+        if (idx == word.size()) return true;
+
+        int row = board.size(), col = board[0].size();
+        if (i < 0 || i >= row || j < 0 || j >= col || board[i][j] != word[idx])
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        bool found = dfs(board, word, i + 1, j, idx + 1) ||
+                     dfs(board, word, i - 1, j, idx + 1) ||
+                     dfs(board, word, i, j + 1, idx + 1) ||
+                     dfs(board, word, i, j - 1, idx + 1);
+
+        board[i][j] = temp;
+        return found;
+    }
+
     bool exist(vector<vector<char>>& board, string word) {
-        int row = board.size();
-        int col = board[0].size();
+        int row = board.size(), col = board[0].size();
+
+        unordered_map<char, int> boardFreq, wordFreq;
+        for (int r = 0; r < row; r++)
+            for (int c = 0; c < col; c++)
+                boardFreq[board[r][c]]++;
+
+        for (char ch : word)
+            wordFreq[ch]++;
+
+        for (auto& [ch, freq] : wordFreq)
+            if (boardFreq[ch] < freq)
+                return false;
+
+        if (boardFreq[word[0]] > boardFreq[word.back()])
+            reverse(word.begin(), word.end());
+
         for (int r = 0; r < row; r++) {
             for (int c = 0; c < col; c++) {
-                if (board[r][c] == word[0] && dfs(board, word, r, c, 0, row, col))
+                if (board[r][c] == word[0] && dfs(board, word, r, c, 0))
                     return true;
             }
         }
+
         return false;
-    }
-
-    bool dfs(vector<vector<char>>& board, string word, int r, int c, int index, int row, int col) {
-        if (index == word.length()) return true;
-        if (r < 0 || c < 0 || r >= row || c >= col || board[r][c] != word[index]) return false;
-
-        char temp = board[r][c];
-        board[r][c] = '*';
-
-        bool find = dfs(board, word, r + 1, c, index + 1, row, col) ||
-                    dfs(board, word, r - 1, c, index + 1, row, col) ||
-                    dfs(board, word, r, c + 1, index + 1, row, col) ||
-                    dfs(board, word, r, c - 1, index + 1, row, col);
-        board[r][c] = temp;
-        
-        return find;
     }
 };
