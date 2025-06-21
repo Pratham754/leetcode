813f56c65ba15f6e0ae891816d374d4c34c1bc74
Pratham Choudhary <113010780+Pratham754@users.noreply.github.com>
2025-06-21 17:13:04 +0530

    Time: 813 ms (30.48%) | Memory: 11.4 MB (54.98%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -5,24 +5,24 @@ public:
         int col = board[0].size();
         for (int r = 0; r < row; r++) {
             for (int c = 0; c < col; c++) {
-                if (dfs(board, word, r, c, 0))
+                if (board[r][c] == word[0] && dfs(board, word, r, c, 0, row, col))
                     return true;
             }
         }
         return false;
     }
 
-    bool dfs(vector<vector<char>>& board, string word, int r, int c, int index) {
+    bool dfs(vector<vector<char>>& board, string word, int r, int c, int index, int row, int col) {
         if (index == word.length()) return true;
-        if (r < 0 || c < 0 || r >= board.size() || c >= board[0].size()) return false;
-        if (board[r][c] != word[index]) return false;
+        if (r < 0 || c < 0 || r >= row || c >= col || board[r][c] != word[index]) return false;
 
         char temp = board[r][c];
         board[r][c] = '*';
-        bool find = dfs(board, word, r + 1, c, index + 1) ||
-                    dfs(board, word, r - 1, c, index + 1) ||
-                    dfs(board, word, r, c + 1, index + 1) ||
-                    dfs(board, word, r, c - 1, index + 1);
+
+        bool find = dfs(board, word, r + 1, c, index + 1, row, col) ||
+                    dfs(board, word, r - 1, c, index + 1, row, col) ||
+                    dfs(board, word, r, c + 1, index + 1, row, col) ||
+                    dfs(board, word, r, c - 1, index + 1, row, col);
         board[r][c] = temp;
         
         return find;
